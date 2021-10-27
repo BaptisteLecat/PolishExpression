60103803e580ba9243cd11fb8fcf23215dad3b36
Baptiste <baptiste.lecat44@gmail.com>
2021-10-27 22:09:57 +0200

    Implement post

--- src/main/java/web/controller/LoginController.java
+++ src/main/java/web/controller/LoginController.java
@@ -13,4 +13,31 @@ public class LoginController extends HttpServlet {
         req.getRequestDispatcher("/WEB-INF/views/auth/login.jsp")
                 .forward(req, resp);
     }
+
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws
+            ServletException, IOException {
+
+        PostChecker
+
+        req.getRequestDispatcher("/WEB-INF/views/auth/login.jsp")
+                .forward(req, resp);
+    }
 }
+
+/*
+
+package utils;
+
+public static class PostChecker {
+
+    public static List<String> checkPostData(Map<String, String> postData){
+        List<String> errorParam = new ArrayList();
+        postData.forEach((name, value) -> {
+            if(value == null || value.equals("")){
+                errorParam.add(name);
+            }
+        });
+        return errorParam;
+    }
+}
+ */
