b27aceb1ca2dbd49484bf74e5707528709c1e38c
Baptiste <baptiste.lecat44@gmail.com>
2021-11-16 19:14:28 +0100

    HomePage

---
+++ src/main/java/web/controller/HomeController.java
@@ -0,0 +1,24 @@
+package web.controller;
+
+import repository.Auth;
+import utils.PostChecker;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class HomeController extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws
+            ServletException, IOException {
+        req.getRequestDispatcher("/WEB-INF/views/home.jsp")
+                .forward(req, resp);
+    }
+}
