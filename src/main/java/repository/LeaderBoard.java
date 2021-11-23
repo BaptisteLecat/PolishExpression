d34bf3d605a079e5d63a61fb3fe476d56b3eb325
Baptiste <baptiste.lecat44@gmail.com>
2021-11-23 22:34:54 +0100

    Sign

--- src/main/java/repository/LeaderBoard.java
+++ src/main/java/repository/LeaderBoard.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Created by BaptisteLecat
+ * Created by BaptisteLecat & FxPelet
  */
 public class LeaderBoard {
 
--- src/main/java/web/controller/ProfilController.java
+++ src/main/java/web/controller/ProfilController.java
@@ -17,7 +17,7 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * Created by BaptisteLecat
+ * Created by BaptisteLecat & FxPelet
  */
 public class ProfilController extends HttpServlet {
 
